5b043448797b5572e7f92cd3836aeb60bcedd631
gin.pan <hereticanthemp@gmail.com>
2023-02-23 23:34:42 +0800

    Add Domain entities

---
+++ Northwind.Domain/Entities/Customer.cs
@@ -0,0 +1,12 @@
+namespace Northwind.Domain.Entities;
+
+public class Customer
+{
+    public string? Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Title { get; set; }
+
+    public string? CompanyName { get; set; }
+}
---
+++ Northwind.Domain/Entities/Employee.cs
@@ -0,0 +1,14 @@
+namespace Northwind.Domain.Entities;
+
+public class Employee
+{
+    public int Id { get; set; }
+
+    public string? FirstName { get; set; }
+    
+    public string? LastName { get; set; }
+
+    public string? Title { get; set; }
+
+    public DateTime BirthDate { get; set; }
+}
