e737cd089fbe3e25ba9644da7bc3ac2652233dd4
gin.pan <hereticanthemp@gmail.com>
2023-06-16 15:06:11 +0800

    Add validation pipeline

---
+++ Northwind.Application/Behaviors/ValidationPipelineBehavior.cs
@@ -0,0 +1,15 @@
+using MediatR;
+using Northwind.Domain.Shared;
+
+namespace Northwind.Application.Behaviors;
+
+public class ValidationPipelineBehavior<TRequest, TResponse>
+    : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+    where TResponse : Result
+{
+    public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
